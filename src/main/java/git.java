eac0ae1ce53b5b7c33f00288a4907782ceda8fc6
zuunun <zuhurunun@MacBook-Air-tilhrende-Zuhur.local>
2024-03-04 09:56:54 +0100

    first commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,6 +2,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("Hej igen");
-        System.out.println("hiii");
+        System.out.println("what");
+
+        System.out.println("Whhhh");
     }
 }
